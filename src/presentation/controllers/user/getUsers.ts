0503c2171f6a027ed9cb7137b9baa8533d4f1857
Rafael Jordão Jardim <jordao.jardim@hotmail.com.br>
2021-07-28 21:37:40 -0300

    fix: change the get user response

--- src/presentation/controllers/user/getUsers.ts
+++ src/presentation/controllers/user/getUsers.ts
@@ -3,8 +3,6 @@ import { IController } from '../../interfaces/controller'
 import { Request, Response } from 'express'
 import { IResponse } from '../../interfaces/response'
 import { IGetUsers } from '../../../domain/user/use-cases/getUsers'
-import { parseToTableFormat } from '../../../utils/common'
-
 export class GetUsersController implements IController {
   constructor (
     private readonly getUsers: IGetUsers) {}
@@ -12,9 +10,6 @@ export class GetUsersController implements IController {
   public async handle (req: Request, res: Response): Promise<IResponse> {
     try {
       const users = await this.getUsers.get()
-      if (users != null) {
-        return ok(parseToTableFormat(users))
-      }
       return ok(users)
     } catch (error) {
       console.error(`Error getting user: ${String(error)}`)
